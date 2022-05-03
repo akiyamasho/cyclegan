b132ed91d29446fb81110ff1c0fdfb47277e751d
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2022-05-04 01:22:27 +0900

    MAINT: add wandb for fid visualisation

--- util/visualizer.py
+++ util/visualizer.py
@@ -7,20 +7,31 @@ from . import util, html
 from subprocess import Popen, PIPE
 
 
+try:
+    import wandb
+except ImportError:
+    print(
+        'Warning: wandb package cannot be found. The option "--use_wandb" will result in error.'
+    )
+
 if sys.version_info[0] == 2:
     VisdomExceptionBase = Exception
 else:
     VisdomExceptionBase = ConnectionError
 
 
-def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
+def save_images(
+    webpage, visuals, image_path, aspect_ratio=1.0, width=256, use_wandb=False
+):
     """Save images to the disk.
+
     Parameters:
         webpage (the HTML class) -- the HTML webpage class that stores these imaegs (see html.py for more details)
         visuals (OrderedDict)    -- an ordered dictionary that stores (name, images (either tensor or numpy) ) pairs
         image_path (str)         -- the string is used to create image paths
         aspect_ratio (float)     -- the aspect ratio of saved images
         width (int)              -- the images will be resized to width x width
+
     This function will save images stored in 'visuals' to the HTML file specified by 'webpage'.
     """
     image_dir = webpage.get_image_dir()
@@ -29,7 +40,7 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
 
     webpage.add_header(name)
     ims, txts, links = [], [], []
-
+    ims_dict = {}
     for label, im_data in visuals.items():
         im = util.tensor2im(im_data)
         image_name = "%s_%s.png" % (name, label)
@@ -38,16 +49,22 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
         ims.append(image_name)
         txts.append(label)
         links.append(image_name)
+        if use_wandb:
+            ims_dict[label] = wandb.Image(im)
     webpage.add_images(ims, txts, links, width=width)
+    if use_wandb:
+        wandb.log(ims_dict)
 
 
 class Visualizer:
     """This class includes several functions that can display/save images and print/save logging information.
+
     It uses a Python library 'visdom' for display, and a Python library 'dominate' (wrapped in 'HTML') for creating HTML files with images.
     """
 
     def __init__(self, opt):
         """Initialize the Visualizer class
+
         Parameters:
             opt -- stores all the experiment flags; needs to be a subclass of BaseOptions
         Step 1: Cache the training/test options
@@ -62,12 +79,15 @@ class Visualizer:
         self.name = opt.name
         self.port = opt.display_port
         self.saved = False
+        self.use_wandb = opt.use_wandb
+        self.current_epoch = 0
+        self.ncols = opt.display_ncols
+
         if (
             self.display_id > 0
         ):  # connect to a visdom server given <display_port> and <display_server>
             import visdom
 
-            self.ncols = opt.display_ncols
             self.vis = visdom.Visdom(
                 server=opt.display_server,
                 port=opt.display_port,
@@ -75,6 +95,16 @@ class Visualizer:
             )
             if not self.vis.check_connection():
                 self.create_visdom_connections()
+
+        if self.use_wandb:
+            self.wandb_run = (
+                wandb.init(
+                    project="CycleGAN-and-pix2pix", name=opt.name, config=opt
+                )
+                if not wandb.run
+                else wandb.run
+            )
+            self.wandb_run._label(repo="CycleGAN-and-pix2pix")
 
         if (
             self.use_html
@@ -96,6 +126,7 @@ class Visualizer:
             log_file.write(
                 "================ Training Loss (%s) ================\n" % now
             )
+
         with open(self.fid_log_name, "a") as log_file:
             now = time.strftime("%c")
             log_file.write(
@@ -120,6 +151,7 @@ class Visualizer:
 
     def display_current_results(self, visuals, epoch, save_result):
         """Display current results on visdom; save current results to an HTML file.
+
         Parameters:
             visuals (OrderedDict) - - dictionary of images to display or save
             epoch (int) - - the current epoch
@@ -191,6 +223,23 @@ class Visualizer:
                 except VisdomExceptionBase:
                     self.create_visdom_connections()
 
+        if self.use_wandb:
+            columns = [key for key, _ in visuals.items()]
+            columns.insert(0, "epoch")
+            result_table = wandb.Table(columns=columns)
+            table_row = [epoch]
+            ims_dict = {}
+            for label, image in visuals.items():
+                image_numpy = util.tensor2im(image)
+                wandb_image = wandb.Image(image_numpy)
+                table_row.append(wandb_image)
+                ims_dict[label] = wandb_image
+            self.wandb_run.log(ims_dict)
+            if epoch != self.current_epoch:
+                self.current_epoch = epoch
+                result_table.add_data(*table_row)
+                self.wandb_run.log({"Result": result_table})
+
         if self.use_html and (
             save_result or not self.saved
         ):  # save images to an HTML file if they haven't been saved.
@@ -243,9 +292,12 @@ class Visualizer:
             )
         except VisdomExceptionBase:
             self.create_visdom_connections()
+        if self.use_wandb:
+            self.wandb_run.log({"epoch": epoch, "fid": fid})
 
     def plot_current_losses(self, epoch, counter_ratio, losses):
         """display the current losses on visdom display: dictionary of error labels and values
+
         Parameters:
             epoch (int)           -- current epoch
             counter_ratio (float) -- progress (percentage) in the current epoch, between 0 to 1
@@ -275,10 +327,13 @@ class Visualizer:
             )
         except VisdomExceptionBase:
             self.create_visdom_connections()
+        if self.use_wandb:
+            self.wandb_run.log(losses)
 
     # losses: same format as |losses| of plot_current_losses
     def print_current_losses(self, epoch, iters, losses, t_comp, t_data):
         """print current losses on console; also save the losses to the disk
+
         Parameters:
             epoch (int) -- current epoch
             iters (int) -- current training iteration during this epoch (reset to 0 at the end of every epoch)
